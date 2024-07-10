546e4230c2ef1ff9709a5eaf241480671e26f998
Sarika06-byte <sarikarana781@gmail.com>
2024-07-10 10:10:02 +0530

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10,b=5; //variable initlisation
+	//BINARY OPERATORS:
+	cout<<"******************************\n";
+	cout<<"BIANRY OPERATORS :";
+	cout<<"\n******************************";
+	cout<<"\n\naddition of two no is "<<a+b;         //addition operators
+	cout<<"\n\nsubtraction of two no is "<<a-b;     //subtraction operators
+	cout<<"\n\nmultitplication of two no is "<<a*b; //multiplication operators
+	cout<<"\n\ndivision of two no is "<<a/b;        //division operators
+	cout<<"\n\nmodulus of tow no is "<<a%b;        //modulus operators
+	//UNARY OPRATORS:
+	cout<<"\n******************************";
+	cout<<"\nUNARY OPRATORS:";
+	cout<<"\n******************************";
+	a++;
+	a++;
+	cout<<"\n\nincrement of (a=10) is "<<a;      //increment operators
+	b--;
+	b--;
+	cout<<"\n\ndecrement of (b=5) no is "<<b;   //decrement operators
+}
